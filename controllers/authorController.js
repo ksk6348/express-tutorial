2bc51a7965ace11db4f4a6ea2c706f59f2deba31
ohya <ksk6348@gmail.com>
2019-07-31 15:04:25 +0900

    create book

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -2,6 +2,8 @@ const Author = require('../models/author');
 const Book = require('../models/book');
 
 const async = require('async');
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 
 exports.author_list = function(req, res) {
   Author.find()
@@ -34,12 +36,42 @@ exports.author_detail = function(req, res) {
 };
 
 exports.author_create_get = function(req, res) {
-  res.send('NOT IMPLEMENTED: Author create get');
+  res.render('author_form', {title: 'Create Author'})
 };
 
-exports.author_create_post = function(req, res) {
-  res.send('NOT IMPLEMENTED: Author create post');
-};
+exports.author_create_post = [
+  body('first_name').isLength({min: 1}).trim().withMessage('First name must be specified.'),
+  body('family_name').isLength({min: 1}).trim().withMessage('Family name must be specified.'),
+  body('date_of_birth', 'Invalid date of birth').optional({checkFalsy: true}).isISO8601(),
+  body('date_of_death', 'Invalid date of death').optional({checkFalsy: true}).isISO8601(),
+
+  sanitizeBody('first_name').escape(),
+  sanitizeBody('family_name').escape(),
+  sanitizeBody('date_of_birth').toDate(),
+  sanitizeBody('date_of_death').toDate(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render('author_form', {title: 'Create Author', author: req.body, errors: errors.array()});
+    }
+    else {
+      const author = new Author(
+        {
+          first_name: req.body.first_name,
+          family_name: req.body.family_name,
+          date_of_birth: req.body.date_of_birth,
+          date_of_death: req.body.date_of_death,
+        }
+      );
+      author.save(function (err) {
+        if (err) {return next(err); }
+        res.redirect(author.url);
+      })
+    }
+  }
+];
 
 exports.author_delete_get = function(req, res) {
   res.send('NOT IMPLEMENTED: Author delete get');
